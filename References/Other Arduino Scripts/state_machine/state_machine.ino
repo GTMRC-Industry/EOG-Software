6bcc5948b1b8057b59214b39f5b99551c8b3eb77
Akhil Ganesan <akhilganesan526@gmail.com>
2026-03-11 15:57:40 -0400

    State Machine (Noiseband + Reset) Implementation

---
+++ References/Other Arduino Scripts/state_machine/state_machine.ino
@@ -0,0 +1,113 @@
+//THE CURRENT SETUP IN THIS CODE PROVIDES 1200 SAMPLES PER SECOND ON A SINGLE ANALOG INPUT (A0)
+//THE OUTPUT IS THE DIGITIZED SIGNAL (0-1023; 10-BIT) AND THE TIME BETWEEN SAMPLES IN MICROSECONDS
+
+#include "Queue.h"
+
+// Sampling Variable Definitions
+const float target_adc_frequency_in_kHz = 1.2;
+const uint8_t analog_pin = 0;
+volatile uint16_t adc_now = 0;
+String serial_string = "";
+volatile unsigned long last_time = 0;
+volatile unsigned long current_time = 0;
+volatile boolean adc_toggle = false;
+
+// Signal Processing Variable Definitions
+Queue<uint16_t> queue = Queue<uint16_t>(5);
+volatile uint16_t signal_threshold = 100;
+volatile uint16_t baseline = 500;
+volatile uint16_t noise_threshold = 5;
+volatile uint16_t output = 0;
+
+void setup()
+{
+  //Disable global interrupts
+  cli();
+  
+  //Baud Rate Setup
+  Serial.begin(230400);
+  
+  // Set up Timer 1 to interrupt at target frequency (for Compare Match B)
+  TCCR1A = 0;// clear register
+  TCCR1B = 0;// clear register
+  TCNT1  = 0;//initialize counter value to 0
+  TCCR1B |= bit (WGM12);// clear timer on match (reset)
+  TCCR1B |= bit (CS10);// set prescaler to 1
+  TIMSK1 |= bit (OCIE1B);// enable timer compare interrupt
+  OCR1B = 16000000 / (target_adc_frequency_in_kHz * 1000);
+  OCR1A = OCR1B;
+  
+  //Initialize Analog Reference (AVcc), Left Adjust, and Analog Pin to Read
+  //With ADLAR bit set, just read ADCH (reduce to 8-bit resolution)
+  ADMUX = bit (REFS0) | (analog_pin & 7);
+ 
+  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to Timer/Counter 1 Compare Match B (1 0 1).
+  // This means ADC will be triggered by Timer 1
+  ADCSRB |= bit (ADTS0) | bit (ADTS2);
+ 
+  ADCSRA = bit (ADEN) // Enable ADC
+          | bit (ADIE) // Enable Interrupt
+          | bit (ADATE); // Enable Auto-Trigger
+ 
+  // Set the Prescaler (16000KHz/128 = 125KHz)
+  // Above 200KHz 10-bit results are not reliable.
+  // About 8-bit resolution up to 1MHz (Prescaler = 16)
+  //ADCSRA |= bit (ADPS0);                               //   2  No precision
+  //ADCSRA |= bit (ADPS1);                               //   4  No precision
+  //ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8  Low precision
+  //ADCSRA |= bit (ADPS2);                               //  16  Okay precision  (up to 77K conversions/s)
+  //ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32 Good precision (up to 38.5K conversions/s)
+  //ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64 Good precision (up to 19.2K conversions/s)
+  ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128 Most precision (up to 9.6K conversions/s)
+  
+  // Enable global interrupts
+  sei();
+}
+
+void loop()
+{
+  if (serial_string.length() > 55)
+  {
+    // Processing (this grabs the ADC reading from the most recent of 8-batched readings)
+    const uint16_t signal = adc_now;
+    // Serial.print(String(signal) + "\n");
+    uint16_t prior = queue.pop();
+    queue.push(signal);
+    uint16_t upper = baseline + signal_threshold;
+    uint16_t lower = baseline - signal_threshold;
+
+    if (signal < upper && signal > lower) {
+      if (abs(signal - prior) < noise_threshold) {
+        baseline = (signal + prior) / 2;
+      }
+    } else if (signal > upper && signal < prior) {
+      output = prior - baseline;
+      Serial.print(String(output));
+    } else if (signal < lower && signal > prior) {
+      output = prior - baseline;
+      Serial.print(String(output));
+    }
+    
+    // Serial Printing
+    // Serial.print(serial_string);
+    serial_string = "";
+  }
+}
+
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect)
+{ 
+  current_time = micros();
+  
+  //For 10-bit resolution (only when operating with a prescaler of 128)
+  adc_now = ADC;// current ADC value
+  //For 8-bit resolution (for operating with a prescaler as low as 16)
+  //adc_now = ADCH;// current ADC value
+
+  //FOR SINGLE INPUT (SIGNAL)
+  serial_string += String(adc_now) + "," + String(current_time - last_time) + "\n";
+  last_time = current_time;
+}
+ISR(TIMER1_COMPB_vect)
+{
+}
